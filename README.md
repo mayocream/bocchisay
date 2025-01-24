536686866cf4dd043c73d428f07ea77528a550f8
Mayo <mayo@linux.com>
2025-01-24 20:43:59 +0900

    Add Clap for command-line argument parsing; update README and Cargo.toml

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # bocchisay
+
 Bocchi rewrite of ponysay.
+
+## Installation
+
+```bash
+cargo install bocchisay
+```
+
+## Usage
+
+```bash
+bocchisay "世界が怖い..."
+```
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[arg(short = 'f', long = "name", default_value = "hitori")]
+    name: Option<String>,
+
+    message: Option<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+    println!("Hello, {}!", args.name.unwrap());
+    if let Some(message) = args.message {
+        println!("{}", message);
+    }
 }
